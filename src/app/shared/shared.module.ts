b149df313c16686a44dce09fa14da0188f15f340
Dmytro KUCHURA <dmytro.kuchura@raiffeisen.ua>
2021-12-13 13:00:32 +0200

    change layout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,18 @@ import {NgModule} from '@angular/core';
 import {BreadcrumbsComponent} from './breadcrumbs/breadcrumbs.component';
 import {NotFoundComponent} from './not-found/not-found.component';
 import {MatComponentsModule} from '../mat-components-module/mat-components.module';
+import {Footer} from './footer/footer';
 
 @NgModule({
   declarations: [
     BreadcrumbsComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    Footer
   ],
   exports: [
-    BreadcrumbsComponent
+    BreadcrumbsComponent,
+    NotFoundComponent,
+    Footer
   ],
   imports: [
     MatComponentsModule
