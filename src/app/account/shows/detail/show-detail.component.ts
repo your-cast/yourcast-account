ea6ec8e3e8fbbc6460d1a3e95cb7f036850224e4
Dmytro Kuchura <kuchura.d@gmail.com>
2022-05-26 20:36:52 +0300

    changes

--- src/app/account/shows/detail/show-detail.component.ts
+++ src/app/account/shows/detail/show-detail.component.ts
@@ -3,6 +3,8 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {ShowService} from '../../../services/show.service';
 import {Show} from '../../../models/show';
 import {AlertService} from '../../../services/alert.service';
+import {EpisodesService} from '../../../services/episodes.service';
+import {forkJoin, Observable} from 'rxjs';
 
 @Component({
   selector: 'app-show-detail',
@@ -11,10 +13,18 @@ import {AlertService} from '../../../services/alert.service';
 })
 export class ShowDetailComponent implements OnInit {
   show: Show;
+  episodesColumns: string[] = ['cover', 'title', 'publish', 'status'];
+  episodesDataSource: [] = [];
+
+  show$: Observable<any>;
+  episodes$: Observable<any>;
+
+  allRequestFinished$: Observable<any>;
 
   constructor(
     private router: Router,
     private showService: ShowService,
+    private episodesService: EpisodesService,
     private activatedRoute: ActivatedRoute,
     private alertService: AlertService
   ) {
@@ -25,9 +35,18 @@ export class ShowDetailComponent implements OnInit {
   }
 
   getData(): void {
-    const id = this.activatedRoute.snapshot.paramMap.get('id');
-    this.showService.getShowInfo(id).subscribe(response => {
-      this.show = response.result;
+    const showId = this.activatedRoute.snapshot.paramMap.get('id');
+
+    this.show$ = this.showService.getShowInfo(showId);
+    this.episodes$ = this.episodesService.showEpisodesList(showId);
+
+    this.allRequestFinished$ = forkJoin([this.show$, this.episodes$]);
+
+    this.allRequestFinished$.subscribe(value => {
+      this.show = value[0].result;
+    }, error => {
+      console.log(error)
+      this.alertService.error('Something want wrong!');
     });
   }
 }
