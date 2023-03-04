a2dfe2fa54a86c584d6b718e95ea2fc1404e2a23
Dmytro Kuchura <kuchura.d@gmail.com>
2023-03-04 16:01:49 +0200

    add dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -44,8 +44,8 @@ import {environment} from '../environments/environment';
       }
     }),
     StoreDevtoolsModule.instrument({
-      maxAge: 25, // Retains last 25 states
-      logOnly: environment.name === 'prod', // Restrict extension to log-only mode
+      maxAge: 25,
+      logOnly: environment.production
     }),
   ],
   bootstrap: [AppComponent],
